5cfc9b587b27ae04e6a19f33e953691101af25d4
tfowler4 <terrijonfowler@gmail.com>
2024-08-18 23:44:27 -0400

    updated shell name

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/react-raid-tracker-frontend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'pm2 restart nodejs-app' >> /home/ec2-user/react-raid-tracker-frontend/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/react-raid-tracker-frontend/deploy.log
+echo 'pm2 restart raid-tracker' >> /home/ec2-user/react-raid-tracker-frontend/deploy.log
+pm2 restart raid-tracker >> /home/ec2-user/react-raid-tracker-frontend/deploy.log
